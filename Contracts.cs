363301602f3acd568a6776e5bb7f31a95aaff286
muhammad <karami8071@yahoo.com>
2025-08-26 18:59:12 +0330

    first commit

---
+++ Contracts.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Contracts;
+public record GrantItems(Guid UserId, Guid CatalogItemId, int Quantity, Guid CorrelationId);
+public record InventoryItemsGranted(Guid CorrelationId);
+public record SubtractItems(Guid UserId, Guid CatalogItemId, int Quantity, Guid CorrelationId);
+public record InventoryItemsSubtracted(Guid CorrelationId);
+public record InventoryItemUpdated(Guid UserId, Guid CatalogItemId, int NewTotalQuantity);
